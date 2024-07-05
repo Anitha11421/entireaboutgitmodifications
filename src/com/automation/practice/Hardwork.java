7282779f4e9f9cf6e76e5d3bafce6ae07093277a
Anitha11421 <anutechmyself12@gmail.com>
2024-07-05 13:40:34 +0530

    modified Hardwork.java

--- src/com/automation/practice/Hardwork.java
+++ src/com/automation/practice/Hardwork.java
@@ -4,6 +4,7 @@ public class Hardwork {
 	public static void main(String[] args) {
 		System.out.println("hardwork= job");
 		System.out.println("subject first job next");
+		System.out.println("hello");
 	}
 
 }
