e977e17bc8a261c078b875eaf7d8bc3e3d1efe4b
Anitha11421 <anutechmyself12@gmail.com>
2024-07-05 12:26:24 +0530

    modified and new file

--- src/com/automation/practice/Hardwork.java
+++ src/com/automation/practice/Hardwork.java
@@ -3,6 +3,7 @@ package com.automation.practice;
 public class Hardwork {
 	public static void main(String[] args) {
 		System.out.println("hardwork= job");
+		System.out.println("subject first job next");
 	}
 
 }
---
+++ src/com/automation/practice/Practice.java
@@ -0,0 +1,7 @@
+package com.automation.practice;
+
+public class Practice {
+public static void main(String[] args) {
+	System.out.println("doing job easy but getting job very dificult");
+}
+}
