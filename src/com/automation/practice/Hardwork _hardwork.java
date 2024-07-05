a0d172c675494eb9bc353c15599199e135cd65e2
Anitha11421 <anutechmyself12@gmail.com>
2024-07-05 22:51:05 +0530

    added to hardwork branch

---
+++ src/com/automation/practice/Hardwork _hardwork.java
@@ -0,0 +1,28 @@
+package com.automation.practice;
+
+public class Hardwork {
+	public static void main(String[] args) {
+		System.out.println("hardwork= job");
+		System.out.println("subject first job next");
+		System.out.println("hello");
+
+		System.out.println("practice is mandotory");
+		System.out.println("welcome to the selenium");
+
+		System.out.println("do practice");
+<<<<<<< HEAD
+
+		System.out.println(".....");
+
+		System.out.println(".....");
+		System.out.println("welcome to the java"); 
+
+=======
+                System.out.println(".....");
+                System.out.println("welcome to the java");
+>>>>>>> 5caf47beafc13d87010a1b32523df9e9eca4af27
+
+
+	}
+
+}
