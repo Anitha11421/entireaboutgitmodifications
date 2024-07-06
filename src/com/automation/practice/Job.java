1f4323cd35ee3214559a806822c7cf4f8262bc8a
Anitha11421 <anutechmyself12@gmail.com>
2024-07-06 17:25:44 +0530

    add the stash code

--- src/com/automation/practice/Job.java
+++ src/com/automation/practice/Job.java
@@ -4,6 +4,9 @@ public class Job {
 	public static void main(String[] args) {
 		System.out.println("hardwork=job");
 		System.out.println("welcome to the java &selenium");
+		
+		System.out.println("practice makes men perfect");
+		;;;;;;;;;;;;;;;
 	}
 
 }
